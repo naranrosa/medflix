a20cc457a37f819b3f9688e69550a7e6536aab2c
naranrosa <natan.rodrigues10@gmail.com>
2025-09-06 11:45:02 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,6 @@
-// src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-// As variáveis serão lidas do ambiente de produção do Cloudflare
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
